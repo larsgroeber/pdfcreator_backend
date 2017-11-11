43a0561c146bc9d6f2cb5fcebabe8e591d1c66cb
Lars Gröber <larsgroeber7@gmail.com>
2017-11-12 00:42:07 +0100

    added better latex error messages

--- API/Services/LatexService.cs
+++ API/Services/LatexService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Configuration;
 
 namespace API.Services
@@ -22,12 +25,20 @@ namespace API.Services
             process.StartInfo.FileName = _latexCommand;
             process.StartInfo.Arguments = arguments;
             process.StartInfo.WorkingDirectory = directory;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
             process.Start();
+            string output = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
 
             if (process.ExitCode != 0)
             {
-                throw new Exception("An error occured while compiling the template!");
+                List<string> outputErrors = new List<string>();
+                foreach (Match match in Regex.Matches(output, "!.*"))
+                {
+                    outputErrors.Add(Regex.Replace(match.Value, "!", String.Empty).Trim());
+                }
+                throw new Exception(outputErrors.Join(" "));
             }
         }
     }
